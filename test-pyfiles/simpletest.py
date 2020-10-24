2beda17730d4bdce15695e1abfc82f1f2a060714
severuscat <yan_os@mail.ru>
2020-10-25 01:20:57 +0300

    py simple test

---
+++ test-pyfiles/simpletest.py
@@ -0,0 +1,15 @@
+x = 11
+def p():
+    while True:
+        x = 110
+        if t == 1:
+            t = 1020
+            t = t + 11
+            pass
+    return x > 3
+w = 91
+if x > 1:
+    x = 2
+w = p()
+s = 1
+s = s + 10 * p
